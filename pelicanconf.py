022c782816b4bfb9c481f4a4ba3b2afe136b2b6f
jackyzy823 <jackyzy823@gmail.com>
2018-08-04 22:33:13 +0800

    implement comment system and improve ui

--- pelicanconf.py
+++ pelicanconf.py
@@ -23,9 +23,9 @@ AUTHOR_FEED_RSS = None
 LINKS = ()
         
 DATE_FORMATS = {
-    'en': '%a, %d %b %Y',
-    'jp': '%Y-%m-%d(%a)',
-    'zh': '%Y年%m月%d日'
+    'en': ('en_US.utf8','%a, %d %b %Y'),
+    'jp': ('ja_JP.utf8','%Y-%m-%d(%a)'),
+    'zh': ('zh_CN.utf8','%Y年%m月%d日')
 }       
       
 
@@ -49,4 +49,55 @@ PAGE_URL = '{slug}.html'
 PAGE_SAVE_AS = '{slug}.html'
 ARTICLE_SAVE_AS = "{category}/{slug}.html"
 ARTICLE_URL = "{category}/{slug}.html"
-THEME = "./mytheme"
+
+ARTICLE_LANG_URL = '{category}/{slug}-{lang}.html'
+ARTICLE_LANG_SAVE_AS = '{category}/{slug}-{lang}.html'
+
+
+
+THEME = "./hikaru"
+
+#Comment system customize
+STATICMAN_API_ENDPOINT = "https://api.staticman.net/v2/entry/jackyzy823/jackyzy823/source/"
+COMMENTS_PATH = "_data/comments"
+
+
+import os
+import os.path
+import json
+
+
+import sys
+import locale
+import six
+
+def readComments(f):
+    with open(f,"r") as c:
+        return json.load(c)
+
+#TODO: sorted
+COMMENTS = { p : sorted([ readComments(os.path.join(COMMENTS_PATH,p,i)) 
+                            for i in os.listdir(os.path.join(COMMENTS_PATH,p)) if os.path.isfile(os.path.join(COMMENTS_PATH,p,i))  
+                        ],key=lambda x:x["date"])    
+                        for p in os.listdir(COMMENTS_PATH) if os.path.isdir(os.path.join(COMMENTS_PATH,p)) }
+print "Comments",COMMENTS
+
+def commentdate(value,articleinfo):
+    from pelican.utils import strftime,SafeDatetime
+    from pelican.settings import DEFAULT_CONFIG
+    if hasattr(articleinfo, 'lang') and articleinfo.lang in DATE_FORMATS:
+        date_format = DATE_FORMATS[articleinfo.lang]
+    else:
+        date_format = DEFAULT_CONFIG['DEFAULT_DATE_FORMAT']
+    if isinstance(date_format, tuple):
+        locale_string = date_format[0]
+        if sys.version_info < (3, ) and isinstance(locale_string,
+                                                    six.text_type):
+            locale_string = locale_string.encode('ascii')
+        locale.setlocale(locale.LC_ALL, locale_string)
+        date_format = date_format[1]
+    return strftime(SafeDatetime.fromtimestamp(value),date_format)
+    pass
+
+JINJA_FILTERS = { 'commentdate' : commentdate }
+#TODO: support comment markdown
